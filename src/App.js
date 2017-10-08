84eb73d3c9d95317c910bb908c5d4400cb163ff9
Kevin Kipp <kevin.kipp@gmail.com>
2017-10-08 12:25:40 -0500

    Remove unsused className

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component<any, any> {
         <div className="row">
           <div className="col-md-6 col-md-offset-3">
             <fieldset>
-              <DynamicForm config={FIELD_CONFIG} className="App-intro" />
+              <DynamicForm config={FIELD_CONFIG} />
             </fieldset>
           </div>
         </div>
