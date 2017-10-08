f517249cc8a29e43485741008b165cba6375d20c
Kevin Kipp <kevin.kipp@gmail.com>
2017-10-08 14:50:13 -0500

    Add react-json-view

--- src/App.js
+++ src/App.js
@@ -6,16 +6,21 @@ import "./App.css";
 import { DynamicForm } from "./DynamicForm/DynamicForm";
 import { FIELD_CONFIG } from "./FieldConfig";
 
+import ReactJson from "react-json-view";
+
 class App extends Component<any, any> {
   render() {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-md-6 col-md-offset-3">
+          <div className="col-md-6">
             <fieldset>
               <legend>Login</legend>
               <DynamicForm config={FIELD_CONFIG} />
             </fieldset>
+          </div>
+          <div className="col-md-6">
+            <ReactJson src={FIELD_CONFIG} />
           </div>
         </div>
       </div>
