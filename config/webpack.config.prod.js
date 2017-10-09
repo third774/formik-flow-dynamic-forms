0a54b2b2e39474150de0ca8bf41d9bb6862659b5
Kevin Kipp <kevin.kipp@gmail.com>
2017-10-09 10:47:00 -0500

    Add BundleAnalyzerPlugin to prod build

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -12,6 +12,7 @@ const eslintFormatter = require("react-dev-utils/eslintFormatter")
 const ModuleScopePlugin = require("react-dev-utils/ModuleScopePlugin")
 const paths = require("./paths")
 const getClientEnvironment = require("./env")
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
@@ -224,6 +225,7 @@ module.exports = {
     ]
   },
   plugins: [
+    new BundleAnalyzerPlugin(),
     // Makes some environment variables available in index.html.
     // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
