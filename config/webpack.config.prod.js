0c8cb6cac5332637c25e6b298fe285024de7c789
Kevin Kipp <kevin.kipp@gmail.com>
2017-10-09 20:02:21 -0500

    Use static analyzer mode

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -225,7 +225,9 @@ module.exports = {
     ]
   },
   plugins: [
-    new BundleAnalyzerPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static"
+    }),
     // Makes some environment variables available in index.html.
     // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
